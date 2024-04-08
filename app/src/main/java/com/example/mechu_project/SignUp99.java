0ee088e2da0365d26626d165ca672af758c09ca6
박시우 <sue020219@gmail.com>
2024-04-08 15:31:35 +0900

    practice3
    연습3

--- app/src/main/java/com/example/mechu_project/SignUp99.java
+++ app/src/main/java/com/example/mechu_project/SignUp99.java
@@ -10,6 +10,6 @@ public class SignUp99 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup3);
-        //ㄴ;ㅇ미라ㅓㄴㅇ미;회;ㄴㅁ얼힘너이헌ㅁ;ㅣ어ㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏ
+        //수정됨1
     }
 }
