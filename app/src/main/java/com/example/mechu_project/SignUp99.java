0f3b2c7318530aa934d7fac54c28909348dd4302
Siwoo <sue020219@gmail.com>
2024-04-13 22:01:46 +0900

    chat기능추가

---
+++ app/src/main/java/com/example/mechu_project/SignUp99.java
@@ -0,0 +1,35 @@
+package com.example.mechu_project;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SignUp99 extends AppCompatActivity {
+
+    Button signupNext;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup1);
+
+        Button signupNext = findViewById(R.id.signupNext);
+
+        // Sign Up 버튼에 클릭 리스너 설정
+        if (signupNext != null) {
+            signupNext.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // SignUp2로
+                    Intent intent = new Intent(SignUp99.this, SignUp2.class);
+                    //fas;dljflasjgl;kasdjf;lhsaldgbjvasdjfl;ksdaj
+                    startActivity(intent);
+                }
+            });
+        }
+
+    }
+}
