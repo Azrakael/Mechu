d3916208e1dffd8cd70e238f2cc23c69669707b3
Siwoo <sue020219@gmail.com>
2024-04-16 22:50:11 +0900

    240416수정

--- app/src/main/java/com/example/mechu_project/SignUp2.java
+++ app/src/main/java/com/example/mechu_project/SignUp2.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,15 +14,19 @@ import android.widget.Toast;
 
 import java.io.UnsupportedEncodingException;
 import java.util.Properties;
+
+import javax.mail.AuthenticationFailedException;
 import javax.mail.Message;
 import javax.mail.MessagingException;
 import javax.mail.PasswordAuthentication;
+import javax.mail.SendFailedException;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 import java.util.Random;
-
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 
 //이메일 인증부분
@@ -57,12 +63,8 @@ public class SignUp2 extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 String recipientEmail = editEmail.getText().toString(); // 사용자가 입력한 이메일 주소를 가져옵니다.
-
-                // 인증번호 생성
-                verificationCode = generateVerificationCode();
-
-                // 이메일 발송
-                new SendMail().execute(recipientEmail, verificationCode);
+                verificationCode = generateVerificationCode(); // 인증번호 생성
+                sendEmail(recipientEmail, verificationCode); // 이메일 발송
             }
         });
 
@@ -98,51 +100,56 @@ public class SignUp2 extends AppCompatActivity {
         return temp.toString();
     }
 
-    // AsyncTask 로 이메일 발송
-    private class SendMail extends AsyncTask<String, Void, Boolean> {
+    private ExecutorService emailExecutor = Executors.newSingleThreadExecutor();
+    private Handler handler = new Handler(Looper.getMainLooper());
 
-        @Override
-        protected Boolean doInBackground(String... params) {
+    private void sendEmail(String recipient, String verificationCode) {
+        emailExecutor.submit(() -> {
             final String username = "hdidvrnd@naver.com"; // 이메일 계정
-            final String password = "Guseotla12@!@"; // 이메일 계정의 비밀번호
+            final String password = "ss"; // 이메일 계정의 비밀번호
 
             Properties props = new Properties();
-            props.put("mail.smtp.host", "smtp.naver.com");      //이메일 서버 호스트 설정
-            props.put("mail.smtp.socketFactory.port", "465");       //smtp서버에서 사용할 소켓 팩토리 포트
-            props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory"); //소켓 팩토리 클래스 설정
-            props.put("mail.smtp.auth", "true");     //smtp인증
-            props.put("mail.smtp.port", "465");         //smtp 서버 포트 설정
+            props.put("mail.smtp.host", "smtp.naver.com");
+            props.put("mail.smtp.socketFactory.port", "465");
+            props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+            props.put("mail.smtp.auth", "true");
+            props.put("mail.smtp.port", "465");
 
-            //이메일 전송에 사용되는 세션
-            Session session = Session.getDefaultInstance(props,
-                    new javax.mail.Authenticator() {
-                        protected PasswordAuthentication getPasswordAuthentication() {
-                            return new PasswordAuthentication(username, password);
-                        }
-                    });
+            Session session = Session.getDefaultInstance(props, new javax.mail.Authenticator() {
+                protected PasswordAuthentication getPasswordAuthentication() {
+                    return new PasswordAuthentication(username, password);
+                }
+            });
 
             try {
                 MimeMessage message = new MimeMessage(session);
-                message.setFrom(new InternetAddress(username, "MECHU Company")); //발신자이름 설정
-                message.addRecipient(Message.RecipientType.TO, new InternetAddress(params[0])); //수신자 설정
+                try {
+                    message.setFrom(new InternetAddress(username, "MECHU Company"));
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                    postResult("발신자 주소 설정 중 에러가 발생했습니다: " + e.getMessage());
+                    return;
+                }
+                message.addRecipient(Message.RecipientType.TO, new InternetAddress(recipient));
                 message.setSubject("인증번호");
-                message.setText("해당 인증번호는 " + params[1] + "입니다.");
+                message.setText("인증번호는 " + verificationCode + "입니다.");
 
                 Transport.send(message);
-                return true;
-            } catch (MessagingException | UnsupportedEncodingException e) {
+                postResult("인증 이메일을 전송했습니다.");
+            } catch (MessagingException e) {
                 e.printStackTrace();
-                return false;
+                if (e instanceof AuthenticationFailedException) {
+                    postResult("인증 오류: 계정 정보를 확인하세요. " + e.getMessage());
+                } else if (e instanceof SendFailedException) {
+                    postResult("이메일 전송 실패: 수신자 주소를 확인하세요. " + e.getMessage());
+                } else {
+                    postResult("이메일 전송에 실패했습니다. 오류: " + e.getMessage());
+                }
             }
-        }
+        });
+    }
 
-        @Override
-        protected void onPostExecute(Boolean result) {
-            if (result) {
-                showMessage("인증 이메일을 전송했습니다.");
-            } else {
-                showMessage("이메일 전송에 실패했습니다. 다시 시도해주세요.");
-            }
-        }
+    private void postResult(String message) {
+        handler.post(() -> showMessage(message));
     }
 }
