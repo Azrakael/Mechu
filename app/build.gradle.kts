6822e43980541b134b6a015cb8f68466855b5ea0
simdaehyeon <146076564+simdaehyeon@users.noreply.github.com>
2024-05-19 16:27:54 +0900

    이미지
    이미지 벡터 이미지가 아닌 glide사용해서 이미지 크기때문에 렉 엄청 걸린듯 지금 렉 안걸리고 스크롤 내리면 레이아웃 짤리는거 수정했고 음식 검색했을때 안나오면 메시지 뜰 수 있게 버그 안걸리게 수정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,7 +40,9 @@ android {
 }
 
 dependencies {
-     implementation ("com.google.android.material:material:1.9.0") //검색 chip
+    implementation ("com.github.bumptech.glide:glide:4.12.0")
+    annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
+    implementation ("com.google.android.material:material:1.9.0") //검색 chip
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.sun.mail:android-mail:1.6.7")  //이메일 smtp
