2327837f07e2c59b47a41e987a90987a39572516
simdaehyeon <146076564+simdaehyeon@users.noreply.github.com>
2024-05-22 18:51:00 +0900

    수정예정
    수저ㅡㅇ

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,7 @@ android {
 
 dependencies {
     implementation ("com.github.bumptech.glide:glide:4.12.0")
+    implementation ("androidx.vectordrawable:vectordrawable-animated:1.1.0")
     annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
     implementation ("de.hdodenhof:circleimageview:3.1.0")
     implementation ("com.google.android.material:material:1.9.0") //검색 chip
--- app/src/main/java/com/example/mechu_project/MenuDetail.java
+++ app/src/main/java/com/example/mechu_project/MenuDetail.java
@@ -1,12 +1,16 @@
 package com.example.mechu_project;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.vectordrawable.graphics.drawable.AnimatedVectorDrawableCompat;
 
 import android.content.Intent;
+import android.graphics.drawable.AnimatedVectorDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 import com.bumptech.glide.Glide;
 import java.io.File;
 
@@ -27,7 +31,6 @@ public class MenuDetail extends AppCompatActivity {
             int fatRatio = intent.getIntExtra("FAT", 0);
             String imagePath = intent.getStringExtra("IMAGE_PATH"); // 이미지 경로 가져오기
 
-
             // 가져온 데이터를 각각의 뷰에 설정
             TextView menuTitle = findViewById(R.id.menuTitle);
             menuTitle.setText(foodName);
@@ -44,36 +47,53 @@ public class MenuDetail extends AppCompatActivity {
             TextView menuCarbohydrate = findViewById(R.id.menuCarbohydrate);
             menuCarbohydrate.setText("탄수화물 함량: " + carbohydrateRatio + "g");
 
-                // 이미지 뷰 설정
-                ImageView menuImage = findViewById(R.id.menuImage);
-                if (imagePath != null) {
-                    File imageFile = new File(imagePath);
-                    if (imageFile.exists()) {
-                        Glide.with(this)
-                                .load(imageFile)
-                                .into(menuImage);
+            // 이미지 뷰 설정
+            ImageView menuImage = findViewById(R.id.menuImage);
+            if (imagePath != null) {
+                File imageFile = new File(imagePath);
+                if (imageFile.exists()) {
+                    Glide.with(this)
+                            .load(imageFile)
+                            .into(menuImage);
 
-                        // 탄수화물 원형 프로그레스 바 및 텍스트 설정
-                        ProgressBar carbsProgressBar = findViewById(R.id.carbsProgressBar);
-                        TextView carbsProgressText = findViewById(R.id.carbsProgressText);
-                        carbsProgressBar.setProgress(carbohydrateRatio); // 탄수화물 비율로 프로그레스 설정
-                        carbsProgressText.setText("탄수화물: " + carbohydrateRatio + "g"); // 탄수화물 비율로 텍스트 설정
+                    // 탄수화물 원형 프로그레스 바 및 텍스트 설정
+                    ProgressBar carbsProgressBar = findViewById(R.id.carbsProgressBar);
+                    TextView carbsProgressText = findViewById(R.id.carbsProgressText);
+                    carbsProgressBar.setProgress(carbohydrateRatio); // 탄수화물 비율로 프로그레스 설정
+                    carbsProgressText.setText("탄수화물: " + carbohydrateRatio + "g"); // 탄수화물 비율로 텍스트 설정
 
-                        // 단백질 원형 프로그레스 바 및 텍스트 설정
-                        ProgressBar proteinProgressBar = findViewById(R.id.proteinProgressBar);
-                        TextView proteinProgressText = findViewById(R.id.proteinProgressText);
-                        proteinProgressBar.setProgress(proteinRatio); // 단백질 비율로 프로그레스 설정
-                        proteinProgressText.setText("단백질: " + proteinRatio + "g"); // 단백질 비율로 텍스트 설정
+                    // 단백질 원형 프로그레스 바 및 텍스트 설정
+                    ProgressBar proteinProgressBar = findViewById(R.id.proteinProgressBar);
+                    TextView proteinProgressText = findViewById(R.id.proteinProgressText);
+                    proteinProgressBar.setProgress(proteinRatio); // 단백질 비율로 프로그레스 설정
+                    proteinProgressText.setText("단백질: " + proteinRatio + "g"); // 단백질 비율로 텍스트 설정
 
-                        // 지방 원형 프로그레스 바 및 텍스트 설정
-                        ProgressBar fatProgressBar = findViewById(R.id.fatProgressBar);
-                        TextView fatProgressText = findViewById(R.id.fatProgressText);
-                        fatProgressBar.setProgress(fatRatio); // 지방 비율로 프로그레스 설정
-                        fatProgressText.setText("지방: " + fatRatio + "g"); // 지방 비율로 텍스트 설정
-
-
+                    // 지방 원형 프로그레스 바 및 텍스트 설정
+                    ProgressBar fatProgressBar = findViewById(R.id.fatProgressBar);
+                    TextView fatProgressText = findViewById(R.id.fatProgressText);
+                    fatProgressBar.setProgress(fatRatio); // 지방 비율로 프로그레스 설정
+                    fatProgressText.setText("지방: " + fatRatio + "g"); // 지방 비율로 텍스트 설정
                 }
             }
         }
+
+
+        ToggleButton dontLoveButton = findViewById(R.id.dontlove);
+
+
+        dontLoveButton.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            // 하트가 금가는 애니메이션 설정
+            if (isChecked) {
+                dontLoveButton.setBackgroundResource(R.drawable.button_broken_heart);
+                Drawable drawable = dontLoveButton.getBackground();
+                if (drawable instanceof AnimatedVectorDrawableCompat) {
+                    ((AnimatedVectorDrawableCompat) drawable).start();
+                } else if (drawable instanceof AnimatedVectorDrawable) {
+                    ((AnimatedVectorDrawable) drawable).start();
+                }
+            } else {
+                dontLoveButton.setBackgroundResource(R.drawable.button_favorite_full);
+            }
+        });
     }
 }
