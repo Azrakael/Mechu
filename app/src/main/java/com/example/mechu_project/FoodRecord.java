170d6cde4b8961ae656e32926a537c5f6241ddd2
simdaehyeon <146076564+simdaehyeon@users.noreply.github.com>
2024-06-01 10:47:26 +0900

    아침점심저녁+버튼클릭시 검색창으로 intent
    sdf

--- app/src/main/java/com/example/mechu_project/FoodRecord.java
+++ app/src/main/java/com/example/mechu_project/FoodRecord.java
@@ -1,21 +1,59 @@
 package com.example.mechu_project;
 
+import android.content.Intent;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.material.bottomsheet.BottomSheetBehavior;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 
 //메뉴추천
 public class FoodRecord extends AppCompatActivity {
 
+    ImageView menuPlus1,menuPlus2,menuPlus3;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_food_record);
+        menuPlus1 = findViewById(R.id.menuPlus1);
+        menuPlus2 = findViewById(R.id.menuPlus2);
+        menuPlus3 = findViewById(R.id.menuPlus3);
+
+
+        menuPlus1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it = new Intent(FoodRecord.this,Search.class);
+                startActivity(it);
+
+            }
+        });
+
+
+        menuPlus2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it = new Intent(FoodRecord.this,Search.class);
+                startActivity(it);
+
+            }
+        });
+        menuPlus3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it = new Intent(FoodRecord.this,Search.class);
+                startActivity(it);
+
+            }
+        });
+
+
+
+
 
         // BottomSheet 설정
         LinearLayout bottomSheet = findViewById(R.id.foodDetailBottomSheet);
